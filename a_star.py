7e4024bb5c22d648c2cdbe963d096f203ae9a2d6
MitchellTurton <mbturton33@gmail.com>
2023-10-15 17:16:35 -0400

    Refactored environment.py and a_star.py

--- a_star.py
+++ a_star.py
@@ -1,156 +1,166 @@
 # Imports
 import numpy as np
-import math
-from typing import List, Tuple
 import time
-
+from typing import List, Tuple
 
 # Heuristic Functions
-def h_dist(start_point: Tuple[int], end_point: Tuple[int]) -> float:
-    return np.sqrt((end_point[0] - start_point[0])**2 + (end_point[1] - start_point[1])**2)
+
+
+def euclidian_dist(start_point: Tuple[int], end_point: Tuple[int]) -> float:
+    """
+    Gives the normal 2d Euclidian distance between 2 points
+    """
+    a = end_point[0] - start_point[0]
+    b = end_point[1] - start_point[1]
+
+    return np.sqrt(a*a + b*b)
 
 
 class AStar():
+    """
+    Implementation of the A* pathfinding algorithm on a grid.
+    """
 
-    def __init__(self, env_grid: np.array, heuristic_func=h_dist) -> None:
+    def __init__(self, env_grid: np.array, heuristic_func=euclidian_dist) -> None:
+        """
+        Initialize the A* algorithm with a given environment grid and heuristic function.
+
+        Args:
+            env_grid (np.array): 2D array representing the environment.
+            heuristic_func (Callable): Function to compute the heuristic. Defaults to euclidian_dist.
+        """
+
+        self.h_func = heuristic_func
+
+        self.update_grid(env_grid)
+
+    def update_grid(self, env_grid):
+        """
+        Update the internal state with a new environment grid.
+
+        Args:
+            env_grid (np.array): 2D array representing the environment.
+        """
+
         self.env_grid: np.array = env_grid
 
-        self.h_func: function = heuristic_func
-
-        self.update_grid()
-
-        self.open_close_map = np.full(self.env_grid.shape, np.NaN)
-        self.open_close_map[self.start_point[0]][self.start_point[1]] = 1
-        self.came_from = np.full(self.env_grid.shape, np.NaN, dtype=np.int32)
-
-        self.g_score = np.full(self.env_grid.shape, np.inf)
-        self.g_score[self.start_point[0]][self.start_point[1]] = 0
-
-        self.f_score = np.full(self.env_grid.shape, np.inf)
-        self.f_score[self.start_point[0]][self.start_point[1]] = self.h_func(
-            self.start_point, self.end_point)
-
-    def update_grid(self):
-        self.start_point: Tuple[int] = None
-        self.end_point: Tuple[int] = None
-
-        print(self.env_grid)
-
-        for row in range(self.env_grid.shape[0]):
-            for col in range(self.env_grid.shape[1]):
-                if self.env_grid[row][col] == 3:
-                    print('new start')
-                    self.start_point = (row, col)
-                elif self.env_grid[row][col] == 4:
-                    print('new end')
-                    self.end_point = (row, col)
-
-                if self.start_point is not None and self.end_point is not None:
-                    break
-            if self.start_point is not None and self.end_point is not None:
-                break
-
-        if self.start_point is None:
-            print('no new start')
+        start_row, start_col = np.where(env_grid == 3)
+        if start_row.size > 0 and start_col.size > 0:
+            self.start_point = (start_row[0], start_col[0])
+        else:
             self.start_point = (0, 0)
 
-        if self.end_point is None:
-            print('no new end')
-            self.end_point = (
-                self.env_grid.shape[0] - 1, self.env_grid.shape[1] - 1)
+        end_row, end_col = np.where(env_grid == 4)
+        if end_row.size > 0 and end_col.size > 0:
+            self.end_point = (end_row[0], end_col[0])
+        else:
+            self.end_point = (env_grid.shape[0] - 1, env_grid.shape[1] - 1)
 
-    def reconstruct_path(self, current=None, came_from: np.array = None) -> List[Tuple[int]]:
-        # print(f"came_from: \n{self.came_from}\n\n")
-        if current is None:
-            current = self.end_point
+        self.open_close_set: np.array = np.full(env_grid.shape, np.NaN)
+        self.open_close_set[self.start_point] = 1
 
-        if came_from is None:
-            came_from = self.came_from
+        self.came_from = np.full(env_grid.shape, np.NaN, dtype=np.int32)
 
-        path = [current]
-        for _ in range(self.env_grid.size):
-            # print(f"i: {current[0]}, j: {current[1]}")
-            # print(f'came_from_val: {self.came_from[current[0]][current[1]]}')
-            current = np.unravel_index(
-                self.came_from[current[0]][current[1]], self.env_grid.shape)
-            # print(f'new_current: {current}')
-            path.append(current)
+        self.g_score = np.full(env_grid.shape, np.inf)
+        self.g_score[self.start_point] = 0
 
-            if current == self.start_point:
-                break
+        self.f_score = np.full(env_grid.shape, np.inf)
+        self.f_score[self.start_point] = self.h_func(
+            self.start_point, self.end_point)
 
-        return path
+    def solve(self, is_logging=False) -> List[Tuple[int]]:
+        """
+        Solve the pathfinding problem using the A* algorithm.
 
-    def step(self) -> np.array:
-        # print("----------------------------------------------------------------")
-        masked_arr = self.f_score.copy()
-        masked_arr[self.open_close_map != 1] = np.Inf
+        Args:
+            is_logging (bool): If True, logs intermediate steps. Defaults to False.
+
+        Returns:
+            List[Tuple[int]]: List of grid positions forming the path from start to goal.
+        """
+
+        while np.any(self.open_close_set == 1):
+            step_grid, done = self.step()
+
+            if is_logging:
+                print(f'Step Grid: \n{step_grid}\n\n')
+
+            if done:
+                return step_grid
+
+        return self.open_close_set
+
+    def step(self) -> np.array:  # Returns an np.array of the current closed and open set
+        """
+        Perform one step of the A* algorithm.
+
+        Returns:
+            np.array: 2D array representing the current state of the open and closed sets.
+        """
+
+        f_score_mask = self.f_score.copy()
+        f_score_mask[self.open_close_set != 1] = np.Inf
+
         current = np.unravel_index(
-            np.argmin(masked_arr), self.env_grid.shape)
-
-        # print(f'\nCurrent: {current}\n\n')
-        # print(f'Mask: \n\n{self.open_close_map == 1}\n\n\n')
-        # print(
-        #     f'Masked: \n\n{masked_arr}\n\n\n')
+            np.argmin(f_score_mask), self.env_grid.shape)
 
         if current == self.end_point:
             path = self.reconstruct_path()
-            output = self.open_close_map.copy()
+            output = self.open_close_set.copy()
 
-            for i, j in path:
-                output[i][j] = 4
+            for pos in path:
+                output[pos] = 4
 
-            return output
+            return output, True
 
-        self.open_close_map[current[0]][current[1]] = -1
+        self.open_close_set[current] = -1
 
         for i in range(-1, 2):
             for j in range(-1, 2):
                 neighbor = (current[0] + i, current[1] + j)
 
                 if (i == 0 and j == 0) or (
-                        neighbor[0] < 0 or neighbor[1] < 0 or
-                        neighbor[0] > self.env_grid.shape[0] - 1 or
-                        neighbor[1] > self.env_grid.shape[1] - 1) or (
-                        self.env_grid[neighbor[0]][neighbor[1]] == -1):
+                    neighbor[0] < 0 or neighbor[1] < 0 or
+                    neighbor[0] >= self.env_grid.shape[0] or
+                    self.env_grid[neighbor] == -1
+                ):
                     continue
 
-                neighbor = (current[0] + i, current[1] + j)
                 grid_dist = 1 if i == 0 or j == 0 else np.sqrt(2)
-                temp_g = self.g_score[current[0]][current[1]] + grid_dist
+                temp_g = self.g_score[current] + grid_dist
 
-                if temp_g < self.g_score[neighbor[0]][neighbor[1]]:
-                    self.came_from[neighbor[0]][neighbor[1]] = np.ravel_multi_index(
+                if temp_g < self.g_score[neighbor]:
+                    self.came_from[neighbor] = np.ravel_multi_index(
                         current, self.env_grid.shape)
 
-                    self.g_score[neighbor[0]][neighbor[1]] = temp_g
+                    self.g_score[neighbor] = temp_g
 
-                    self.f_score[neighbor[0]][neighbor[1]] = temp_g + \
+                    self.f_score[neighbor] = temp_g + \
                         self.h_func(neighbor, self.end_point)
 
-                    self.open_close_map[neighbor[0]][neighbor[1]] = 1
+                    self.open_close_set[neighbor] = 1
 
-        # print(f'open_close_map: \n\n{self.open_close_map}\n\n\n')
-        # print(f'f_score       : \n\n{self.f_score}\n\n\n')
-        # print(f'g_score       : \n\n{self.g_score}\n\n\n')
+        return self.open_close_set, False
 
-        return self.open_close_map
+    def reconstruct_path(self) -> List[Tuple[int]]:
+        """
+        Reconstruct the path from the goal to the start using the internal state.
 
-    def solve(self, disp_env=None) -> List[Tuple[int]]:
-        while np.any(self.open_close_map == 1):
-            step_grid = self.step()
-            print(step_grid)
-            print("\n\n")
-            if disp_env is not None:
-                disp_env.overlay_grid = step_grid
-            else:
-                # print(step_grid)
-                # print()
-                pass
+        Returns:
+            List[Tuple[int]]: List of grid positions forming the path from start to goal.
+        """
 
-            time.sleep(0.125)
+        current = self.end_point
+        path = [current]
+
+        while path[-1] != self.start_point:
+            current = np.unravel_index(
+                self.came_from[current], self.env_grid.shape)
+            path.append(current)
+
+        return path
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     a_star = AStar(np.zeros((5, 5)))
-    a_star.solve()
+    a_star.solve(is_logging=True)
--- environment.py
+++ environment.py
@@ -1,10 +1,11 @@
-# Importts
+# Imports
 import pygame
 import numpy as np
-import math
-from a_star import *
+from a_star import AStar
 
-# Initializng pygame
+from typing import Tuple
+
+# Initialize pygame
 pygame.init()
 
 # Color Constants
@@ -16,175 +17,147 @@ BLUE = (0, 157, 196)
 ORANGE = (255, 165, 0)
 
 # Grid Square Types
-OPEN_SPACE = 0
+EMPTY_SQUARE = 0
 OBSTACLE = -1
-SEARCHED = 1
-EVALUATED = 2
+OPEN_SET = 1
+CLOSED_SET = 2
 START = 3
 GOAL = 4
 
-
-# Dictionaries mapping grid type to color
-color_dict = {OPEN_SPACE: WHITE,
+# Dictionary that maps grid type to color to display in environment
+COLOR_DICT = {EMPTY_SQUARE: WHITE,
               OBSTACLE: BLACK,
-              SEARCHED: GREEN,
-              EVALUATED: RED,
+              OPEN_SET: GREEN,
+              CLOSED_SET: RED,
               START: BLUE,
               GOAL: ORANGE
               }
 
+FPS = 100
+
 
 class BasicGridEnv():
     """
-    Class containing a basic representation of a grid world environment,
-    as well as methods to display and generate this environment.
+    Represents a basic grid environment for path planning simulations.
     """
 
-    def __init__(self, win_title: str = "Grid", win_width: int = 800,
-                 win_height: str = 800, num_rows: int = 25, num_cols: int = 25, obstacle_density: float = 0.0) -> None:
+    def __init__(self, win_title: str = "Grid", win_width: int = 800, win_height: int = 800,
+                 num_rows: int = 25, num_cols: int = 25, obstacle_density: float = 0.0) -> None:
         """
-        Initialize the grid environment class.
-
-        Args:
-            win_title (str, optional): Title of screen. Defaults to "Grid".
-            win_width (int, optional): Width of screen. Defaults to 800.
-            win_height (str, optional): Height of screen. Defaults to 800.
-            num_rows (int, optional): Num of rows of squares. Defaults to 25.
-            num_cols (int, optional): Num of columns of squares. Defaults to 25.
-            obstacle_density (float, optional): Percentage of obstacles to open spaces. Defaults to 0.0.
+        Initializes the grid environment with given parameters.
         """
 
-        self.win_width = win_width
-        self.win_height = win_height
-        self.num_rows = num_rows
-        self.num_cols = num_cols
-        self.square_width = self.win_width // self.num_cols
-        self.square_height = self.win_height // self.num_rows
+        # -------------- Logic Variables ---------------
+        self.grid: np.array = self.generate_random_grid(
+            num_rows, num_cols, obstacle_density)
+        # TODO: refactor the overlay grid system
 
-        self.obstacle_density = obstacle_density
-        self.editor_square_type = OBSTACLE
+        self.obstacle_density: int = obstacle_density
 
-        self.grid = self.generate_random_grid()
-        self.overlay_grid = None
+        # For a simple editor, tells the type that the grid square will be replaced with upon clicking
+        self.edit_square_type: int = OBSTACLE
 
-        self.screen = pygame.display.set_mode(
-            (self.win_width, self.win_height))
+        self.path_planner = None
+
+        # ------------- Graphics Variables -------------
+        self.screen = pygame.display.set_mode((win_width, win_height))
         pygame.display.set_caption(win_title)
         self.clock = pygame.time.Clock()
 
-    def draw(self):
+        self.square_width: int = win_width // num_cols
+        self.square_height: int = win_height // num_rows
+
+        self.run_sim = True
+        self.mouse_down = False
+        self.is_path_planning = False
+
+    def generate_random_grid(self, num_rows: int, num_cols: int, obstacle_density: float = None) -> np.array:
         """
-        Visualizes a representation of the grid environment
-        """
-
-        display_grid = self.grid
-
-        if self.overlay_grid is not None:
-            for i in range(self.overlay_grid.shape[0]):
-                for j in range(self.overlay_grid.shape[1]):
-                    if self.overlay_grid[i][j] == 1 and self.grid[i][j] != OBSTACLE:
-                        display_grid[i][j] = SEARCHED
-                    if self.overlay_grid[i][j] == -1 and self.grid[i][j] != OBSTACLE:
-                        display_grid[i][j] = EVALUATED
-                    if self.overlay_grid[i][j] == 4 and self.grid[i][j] != OBSTACLE:
-                        display_grid[i][j] = START
-
-        self.screen.fill(WHITE)
-        for i in range(display_grid.shape[0]):
-            for j in range(display_grid.shape[1]):
-                color = color_dict[display_grid[i][j]]
-
-                pygame.draw.rect(self.screen, color,
-                                 (i * self.square_height, j * self.square_width,
-                                  self.square_height, self.square_width), 0)
-
-                pygame.draw.rect(self.screen, BLACK,
-                                 (i * self.square_height, j * self.square_width,
-                                  self.square_height, self.square_width), 1)
-
-        pygame.display.flip()
-
-        self.clock.tick(100)
-
-    def mouse_handler(self):
-        """
-        Handles user mouse input, used to edit the environment
-        """
-
-        mouse_pos = pygame.mouse.get_pos()
-        row = math.floor(mouse_pos[0] / self.square_width)
-        col = math.floor(mouse_pos[1] / self.square_height)
-        self.grid[row][col] = self.editor_square_type
-
-    def generate_random_grid(self, obstacle_density: float = None) -> np.array:
-        """
-        Using the current grid size, create a new random grid of clear spaces
-        and obstacles.
-
-        Args:
-            obstacle_density (float, optional): 
-               -Percentage of spaces that are obstacles in the grid compared to clear 
-                spaces. 
-               -Defaults to None.
-                   -If None sets obstacle density to class obstacle density
+        Generates a random grid based on given parameters.
         """
 
         if obstacle_density is None:
             obstacle_density = self.obstacle_density
 
-        output = np.random.choice([OPEN_SPACE, OBSTACLE], size=(
-            self.num_rows, self.num_cols), p=[1 - obstacle_density, obstacle_density])
+        return np.random.choice([EMPTY_SQUARE, OBSTACLE], size=(num_rows, num_cols),
+                                p=(1-obstacle_density, obstacle_density))
 
-        return output
+    def event_handler(self) -> None:
+        """
+        Handles pygame events like quitting, mouse clicks, and key presses.
+        """
 
-
-if __name__ == "__main__":
-    env = BasicGridEnv("Basic Grid", 800, 800, 25, 25, 0.25)
-    a_star = AStar(env.grid)
-
-    mouse_down = False
-    run = True
-    run_a_star = False
-    while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                run = False
+                self.run_sim = False
 
-            elif event.type == pygame.MOUSEBUTTONDOWN:
-                mouse_down = True
-
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                self.mouse_down = True
             elif event.type == pygame.MOUSEBUTTONUP:
-                mouse_down = False
+                self.mouse_down = False
 
-            elif event.type == pygame.KEYDOWN:
-                keys = pygame.key.get_pressed()
-                if keys[pygame.K_SPACE]:
-                    env.editor_square_type = OBSTACLE
-                if keys[pygame.K_1]:
-                    env.editor_square_type = SEARCHED
-                if keys[pygame.K_2]:
-                    env.editor_square_type = EVALUATED
-                if keys[pygame.K_3]:
-                    env.editor_square_type = START
-                if keys[pygame.K_4]:
-                    env.editor_square_type = GOAL
-                if keys[pygame.K_0]:
-                    env.editor_square_type = OPEN_SPACE
-                if keys[pygame.K_TAB]:
-                    run_a_star = True
-                if keys[pygame.K_r]:
-                    env.generate_random_grid()
+            if event.type == pygame.KEYDOWN:
+                self.keyboard_handler()
 
-        if mouse_down:
-            env.mouse_handler()
+        if self.mouse_down:
+            self.mouse_handler()
 
-        if run_a_star:
-            if np.any(a_star.open_close_map):
-                step_grid = a_star.step()
-                # print(step_grid)
-                # print("\n\n")
-                env.overlay_grid = step_grid
+    def keyboard_handler(self) -> None:
+        """
+        Handles key press events.
+        """
 
-        env.draw()
+        keys = pygame.ket.get_pressed()
 
-    pygame.quit()
+        if keys[pygame.K_SPACE]:
+            self.edit_square_type = OBSTACLE if self.edit_square_type == EMPTY_SQUARE else EMPTY_SQUARE
+        if keys[pygame.K_1]:
+            self.edit_square_type = START
+        if keys[pygame.K_2]:
+            self.edit_square_type = GOAL
+        if keys[pygame.K_KP_ENTER]:
+            self.is_path_planning = True
+            self.path_planner = AStar(self.grid)
+
+        if keys[pygame.K_r]:
+            self.generate_random_grid(self.grid.shape[0], self.grid.shape[1])
+
+    def mouse_handler(self):
+        """
+        Handles mouse click events to edit the grid.
+        """
+
+        mouse_pos: Tuple[int] = pygame.mouse.get_pos()
+        grid_pos: Tuple[int] = (np.floor(mouse_pos[0] / self.square_width),
+                                np.floor(mouse_pos[1] / self.square_height))
+
+        self.grid[grid_pos] = self.editor_square_type
+
+    def draw(self) -> None:
+        """
+        Renders the grid on the pygame surface.
+        """
+
+        self.screen.fill(WHITE)
+
+        for row in range(self.grid.shape[0]):
+            for col in range(self.grid.shape[1]):
+                square_color = COLOR_DICT[self.grid[row][col]]
+                square_params = (row * self.square_height, col * self.square_width,
+                                 self.square_height, self.square_width)
+
+                pygame.draw.rect(self.screen, square_color, square_params, 0)
+                pygame.draw.rect(self.screen, BLACK, square_params, 1)
+
+        pygame.display.flip()
+
+        self.clock.tick(FPS)
+
+    def update(self) -> bool:
+        """
+        Updates the grid environment, handles events, and draws the grid.
+        """
+
+        self.event_handler()
+
+        output = self.path_planner.step()  # TODO: Think on this interface more
